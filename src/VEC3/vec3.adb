598d281202bf979707935b49639804e56e8d66f7
Travis Betz <travisaidanbetz@gmail.com>
2025-08-23 12:34:38 -0400

    Fix accessing of properties in VECTOR3 type

--- src/VEC3/vec3.adb
+++ src/VEC3/vec3.adb
@@ -1,48 +1,49 @@
-with Ada.Numerics;
+
+with Ada.Numerics.Long_Elementary_Functions;
 
 package body VEC3 is 
 
    -- Use types
-   use Ada.Numerics;
+   use Ada.Numerics.Long_Elementary_Functions;
 
    -- Constructor function
    function Init_Vec3(X, Y, Z : Long_Float) return VECTOR3 is 
    begin 
-      return ( E => (0 => X, 1 => Y, 2 => Z));
+      return ( V3 => (0 => X, 1 => Y, 2 => Z));
    end Init_Vec3;
 
    -- Functions
    -- Getters
    function Get_X(V: VECTOR3) return Long_Float is 
    begin 
-      return V.X;
+      return V.V3(0);
    end Get_X;
 
    function Get_Y(V: VECTOR3) return Long_Float is
    begin 
-      return V.Y;
+      return V.V3(1);
    end Get_Y;
 
    function Get_Z(V: VECTOR3) return Long_Float is 
    begin 
-      return V.Z;
+      return V.V3(2);
    end Get_Z;
 
    function Get_Length(V: VECTOR3) return Long_Float is 
    begin 
-      return Sqrt(V.X * V.X + V.Y * V.Y + V.Z * V.Z);
+      return Sqrt(V.V3(0) * V.V3(0) + V.V3(1) * V.V3(1) + V.V3(2) * V.V3(2));
    end Get_Length;
 
    function Get_Unit_Vector(V: VECTOR3) return VECTOR3 is 
       Length : Long_Float;
    begin 
       Length := Get_Length (V);
-      return (Init_Vec3 (X => V.X / Length, Y => V.Y / Length, Z => V.Z / Length));
+      return (Init_Vec3 (X => V.V3(0) / Length, Y => V.V3(1) / Length, Z => V.V3(2) / Length));
    end Get_Unit_Vector;
 
    function Get_Unit_Vector(V: VECTOR3; F: Long_Float) return VECTOR3 is 
    begin 
-      return (Init_Vec3 (X => V.X / F, Y => V.Y / F, Z => V.Z / F));
+      return (Init_Vec3 (X => V.V3(0) / F, Y => V.V3(1) / F, Z => V.V3(2) / F));
    end Get_Unit_Vector;
 
 
--- src/main.adb
+++ src/main.adb
@@ -8,6 +8,9 @@ procedure Main is
 begin
    --  Generate a PPM for fun if it doesn't exist beforehand
    PPM.PPM_GEN;
+
+   -- Create a test vector and print it
    Test := VEC3.Init_Vec3(X => 10.0, Y => 15.6, Z => 21.4);
+   --VEC3.Print_Vector(Test);
 
 end Main;
