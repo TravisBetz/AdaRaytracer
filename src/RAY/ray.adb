ea76349c07e018747cf2e4fc9ce368af5841764d
Travis Betz <travisaidanbetz@gmail.com>
2025-10-20 21:52:54 -0400

    Update Ray class with new getters

--- src/RAY/ray.adb
+++ src/RAY/ray.adb
@@ -1,13 +1,30 @@
 with VEC3;
 
-package RAY is 
+package body RAY is 
 
-   function Get_Distance_Along_Ray(Ray : in Ray;
-                                   t   : in Float) is
-   tD : VEC3;
+   function Init_Ray(Orig : in VEC3.VECTOR3;
+                     Dir  : in VEC3.VECTOR3) return RAY is 
+   begin
+      return ((Origin => Orig, 
+               Direction => Dir));
+   end Init_Ray;
+
+   function Get_Direction(R : in RAY) return VEC3.VECTOR3 is 
    begin 
-      tD := VEC3.Scale_By_Factor (Ray.Direction, t);
-      return VEC3.Plus (tD, Ray.Origin);
+      return R.Direction;
+   end Get_Direction;
+
+   function Get_Origin(R : in RAY) return VEC3.VECTOR3 is 
+   begin 
+      return R.Origin;
+   end Get_Origin;
+
+   function Get_Distance_Along_Ray(R   : in Ray;
+                                   t   : in Long_Float) return VEC3.VECTOR3 is
+   tD : VEC3.VECTOR3;
+   begin 
+      tD := VEC3.Scale(R.Direction, t);
+      return VEC3.Plus(tD, R.Origin);
    end Get_Distance_Along_Ray;
 
 end RAY;
--- src/RAY/ray.ads
+++ src/RAY/ray.ads
@@ -7,9 +7,16 @@ package RAY is
       Direction : VEC3.VECTOR3;
    end record;
 
+   function Init_Ray(Orig : in VEC3.VECTOR3;
+                     Dir  : in VEC3.VECTOR3) return RAY;
+
+   function Get_Direction(R : in RAY) return VEC3.VECTOR3;
+
+   function Get_Origin(R : in RAY) return VEC3.VECTOR3;
+
    -- Returns current position along ray.
-   function Get_Distance_Along_Ray(Ray : in RAY; 
-                                   t   : in Float) return VEC3.VECTOR3;
+   function Get_Distance_Along_Ray(R   : in RAY; 
+                                   t   : in Long_Float) return VEC3.VECTOR3;
 
 
 end RAY;
